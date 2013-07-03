a268d4865593659124be7bcea65742a738b12486
Marco Hinz <mh.codebro@gmail.com>
2013-07-03 17:23:38 +0200

    Add showcolors.bash

---
+++ showcolors.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! hash tput 2>/dev/null; then
+    echo "I cannot find the 'tput' program." \
+        'You might find it in one of the ncurses packages.' >&2
+    exit 1
+fi
+
+for i in {0..255}; do
+    tput setab $i && echo -n " $i "
+done
