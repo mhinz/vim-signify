mhinz/vim-signify

commit 7f57ee5af62cde3eafca84baed589b240bb43ce8

Use two digit numbers for SignifyDelete signs
Defining the SignifyDelete set of signs on the fly provides the ability
to specify the exact line deletion count for up to 99 lines, rather than
the current limit of 9 lines.

This also has the benefit of reducing the number of signs defined when
there aren't many hunks of deleted lines.  On the flip side, if there
are other sign-placing plugins in use, then it also increases the chance
of nearing the ~120 sign definition limit if there are many hunks of
varying line deletions.

autoload/sy/repo.vim
@@ -220,7 +220,8 @@ function! sy#repo#process_diff(diff) abort
               \ 'lnum': 1 })
       else
         call add(signs, {
-              \ 'type': (old_count > 9) ? 'SignifyDeleteMore' : 'SignifyDelete'. old_count,
+              \ 'type': (old_count > 99) ? 'SignifyDeleteMore' : 'SignifyDelete',
+              \ 'count': old_count,
               \ 'lnum': new_line })
       endif
 

autoload/sy/sign.vim
@@ -2,6 +2,10 @@
 
 scriptencoding utf-8
 
+" Init: values {{{1
+let s:sign_delete = get(g:, 'signify_sign_delete', '_')
+let s:delete_highlight = ['', 'SignifyLineDelete']
+
 " Function: #get_others {{{1
 function! sy#sign#get_others() abort
   let s:other_signs_line_numbers = {}
@@ -38,7 +42,12 @@ function! sy#sign#set(signs)
     endif
 
     call add(hunk.ids, g:id_top)
-    execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type 'buffer='. b:sy.buffer
+    if sign.type == 'SignifyDelete'
+      execute 'sign define SignifyDelete'. sign.count .' text='. (s:sign_delete . sign.count)[-2:] .' texthl=SignifySignDelete linehl='. s:delete_highlight[g:signify_line_highlight]
+      execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type . sign.count 'buffer='. b:sy.buffer
+    else
+      execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type 'buffer='. b:sy.buffer
+    endif
 
     let g:id_top += 1
   endfor

autoload/sy/highlight.vim
@@ -4,7 +4,6 @@ scriptencoding utf-8
 
 " Init: values {{{1
 let s:sign_add               = get(g:, 'signify_sign_add',               '+')
-let s:sign_delete            = get(g:, 'signify_sign_delete',            '_')
 let s:sign_delete_first_line = get(g:, 'signify_sign_delete_first_line', '‾')
 let s:sign_change            = get(g:, 'signify_sign_change',            '!')
 
@@ -35,17 +34,6 @@ function! sy#highlight#line_enable() abort
   execute 'sign define SignifyChangeDelete9 text='. s:sign_change .'9 texthl=SignifySignChange linehl=SignifyLineChange'
   execute 'sign define SignifyChangeDeleteMore text='. s:sign_change .'> texthl=SignifySignChange linehl=SignifyLineChange'
 
-  execute 'sign define SignifyDelete1 text='. s:sign_delete .'1 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete2 text='. s:sign_delete .'2 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete3 text='. s:sign_delete .'3 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete4 text='. s:sign_delete .'4 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete5 text='. s:sign_delete .'5 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete6 text='. s:sign_delete .'6 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete7 text='. s:sign_delete .'7 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete8 text='. s:sign_delete .'8 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDelete9 text='. s:sign_delete .'9 texthl=SignifySignDelete linehl=SignifyLineDelete'
-  execute 'sign define SignifyDeleteMore text='. s:sign_delete .'> texthl=SignifySignDelete linehl=SignifyLineDelete'
-
   execute 'sign define SignifyDeleteFirstLine text='. s:sign_delete_first_line ' texthl=SignifySignDelete linehl=SignifyLineDelete'
 
   let g:signify_line_highlight = 1
@@ -66,17 +54,6 @@ function! sy#highlight#line_disable() abort
   execute 'sign define SignifyChangeDelete8 text='. s:sign_change .'8 texthl=SignifySignChange linehl='
   execute 'sign define SignifyChangeDelete9 text='. s:sign_change .'9 texthl=SignifySignChange linehl='
   execute 'sign define SignifyChangeDeleteMore text='. s:sign_change .'> texthl=SignifySignChange linehl='
-
-  execute 'sign define SignifyDelete1 text='. s:sign_delete .'1 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete2 text='. s:sign_delete .'2 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete3 text='. s:sign_delete .'3 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete4 text='. s:sign_delete .'4 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete5 text='. s:sign_delete .'5 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete6 text='. s:sign_delete .'6 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete7 text='. s:sign_delete .'7 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete8 text='. s:sign_delete .'8 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDelete9 text='. s:sign_delete .'9 texthl=SignifySignDelete linehl='
-  execute 'sign define SignifyDeleteMore text='. s:sign_delete .'> texthl=SignifySignDelete linehl='
 
   execute 'sign define SignifyDeleteFirstLine text='. s:sign_delete_first_line ' texthl=SignifySignDelete linehl='
 

commit 463a4361661521649c3a049ad410fdfc50b4a268

Restore behavior of using _> for SignifyDeleteMore signs
Regression was introduced by 7f57ee5a.

Signed-off-by: James McCoy <vega.james@gmail.com>

autoload/sy/repo.vim
@@ -4,6 +4,7 @@ scriptencoding utf-8
 
 " Init: values {{{1
 
+let s:sign_delete = get(g:, 'signify_sign_delete', '_')
 if !exists('g:signify_diffoptions')
   let g:signify_diffoptions = {}
 endif
@@ -218,11 +219,16 @@ function! sy#repo#process_diff(diff) abort
         call add(signs, {
               \ 'type': 'SignifyDeleteFirstLine',
               \ 'lnum': 1 })
+      elseif old_count <= 99
+        call add(signs, {
+              \ 'type': 'SignifyDelete'.old_count,
+              \ 'text': substitute(s:sign_delete . old_count, '.*\ze..$', '', ''),
+              \ 'lnum': new_line })
       else
         call add(signs, {
-              \ 'type': 'SignifyDelete',
-              \ 'count': old_count,
-              \ 'lnum': new_line })
+              \ 'type': 'SignifyDeleteMore',
+              \ 'lnum': new_line,
+              \ 'text': s:sign_delete . '>' })
       endif
 
     " 2 lines changed:

autoload/sy/sign.vim
@@ -3,7 +3,6 @@
 scriptencoding utf-8
 
 " Init: values {{{1
-let s:sign_delete = get(g:, 'signify_sign_delete', '_')
 let s:delete_highlight = ['', 'SignifyLineDelete']
 
 " Function: #get_others {{{1
@@ -42,10 +41,9 @@ function! sy#sign#set(signs)
     endif
 
     call add(hunk.ids, g:id_top)
-    if sign.type == 'SignifyDelete'
-      let sygn = (sign.count < 10) ? (s:sign_delete . sign.count) : string(sign.count)[-2:]
-      execute 'sign define SignifyDelete'. sign.count .' text='. sygn .' texthl=SignifySignDelete linehl='. s:delete_highlight[g:signify_line_highlight]
-      execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type . sign.count 'buffer='. b:sy.buffer
+    if sign.type =~# 'SignifyDelete'
+      execute 'sign define '. sign.type .' text='. sign.text .' texthl=SignifySignDelete linehl='. s:delete_highlight[g:signify_line_highlight]
+      execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type 'buffer='. b:sy.buffer
     else
       execute 'sign place' g:id_top 'line='. sign.lnum 'name='. sign.type 'buffer='. b:sy.buffer
     endif